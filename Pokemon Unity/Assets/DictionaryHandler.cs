ecb6cb0be297d499a10c9c6ff57be3057b19ffdd
BuildTools <unconfigured@null.spigotmc.org>
2018-11-28 04:44:04 -0500

    changed dictionary words

--- Pokemon Unity/Assets/DictionaryHandler.cs
+++ Pokemon Unity/Assets/DictionaryHandler.cs
@@ -7,10 +7,10 @@ public class DictionaryHandler : MonoBehaviour {
     [SerializeField]
     public Dictionary<string, string> dict = new Dictionary<string, string>();
     public string[] english = { "looking for", "so amazing", "the computer", "walking", "door", "parents", "I don't like", "the school",
-                                "the doctors", "the city", "bed", "the fridge", "video games", "the TV", "This bookshelf"};
+                                "the doctors", "the city", "bed", "the fridge", "video games", "the TV", "This bookshelf", "the trash"};
 
-    public string[] spanish = { "buscando", "muy genial", "la computadora", "caminando", "puerta", "padres", "no me gusta", "la escuela",
-                                "los doctores", "la ciudad", "cama", "la nevera", "videojuegos", "la televisión", "Esta estantería"};
+    public string[] spanish = { "buscando", "tan increíble", "la computadora", "caminando", "puerta", "padres", "no me gusta", "la escuela",
+                                "los doctores", "la ciudad", "cama", "la nevera", "videojuegos", "la televisión", "Esta estantería", "la basura"};
 
 
     public void addToDict(string key, string value)
