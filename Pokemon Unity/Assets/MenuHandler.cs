c61e6f488ce28cd30442548518667c2e41932053
BuildTools <unconfigured@null.spigotmc.org>
2018-12-01 23:27:55 -0500

    added dictionary toggling

--- Pokemon Unity/Assets/MenuHandler.cs
+++ Pokemon Unity/Assets/MenuHandler.cs
@@ -1,19 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour {
+    bool open = false;
+    public Canvas Canvas;
+    public Button[] buttons;
+    private DictionaryHandler dictHandler;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    void Start () {
+        dictHandler = GameObject.Find("GUI").GetComponent<DictionaryHandler>();
+
+        if (transform.GetComponent<DictionaryHandler>() != null)
+        {
+            dictHandler = transform.GetComponent<DictionaryHandler>();
+        }
+
+
+       
+        /*button.onClick.AddListener(() => OnButtonClick(1+1
+       print("lol"));*/
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.V))
+		if (Input.GetKeyDown(KeyCode.P))
         {
-            print("menu opened!");
+            open = !open;
+            Canvas.gameObject.SetActive(open);
         }
-	}
+
+        int i = 0;
+        foreach (KeyValuePair<string, string> entry in dictHandler.getDict())
+        {
+            buttons[i].GetComponent<Button>().gameObject.SetActive(true);
+            buttons[i].GetComponentInChildren<Text>().text = entry.Value;
+            i++;
+        }        
+    }
 }
